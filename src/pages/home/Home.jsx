2eae2802f1e7d812496d480adf29639debc29b26
Sumanbitw <007sumanghosh@gmail.com>
2021-05-28 22:56:56 +0530

    added home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
     return (
         <div>
-            This is home page page
+            This is home page page and this is for social media app for roc8
         </div>
     )
 }
